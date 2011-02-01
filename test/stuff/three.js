df85302e4e722678ceb0ca458835353e983ce7b5
unscriptable <john@e-numera.com>
2011-01-31 22:33:06 -0500

    added tests for paths

--- test/stuff/three.js
+++ test/stuff/three.js
@@ -1,6 +1,6 @@
 /*
  * stuff/three depends on stuff/one and stuff/two
  */
-define(['./one', './two', './zero'], function (one, two) {
+define(['./one', './two', 'stuff/zero'], function (one, two) {
 	return one + two;
 });
