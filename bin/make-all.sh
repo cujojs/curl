c4566d74270fc578f6006cb4c62beceb7c9db1fb
unscriptable <john@e-numera.com>
2011-05-17 23:49:00 -0400

    added dojo 1.6 build

--- bin/make-all.sh
+++ bin/make-all.sh
@@ -3,3 +3,4 @@
 
 # make other versions of curl
 ./make.sh ../dist/curl-with-js-and-domReady/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/plugin/js.js
+./make.sh ../dist/curl-for-dojo1.6/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/dojo16Compat.js
