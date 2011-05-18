9e384cc729b896cb1faff33a4bc6ac5f76ab07e3
unscriptable <john@e-numera.com>
2011-05-18 00:11:45 -0400

    added new dojo build and added domReady plugin to builds (not just domReady module)

--- bin/make-all.sh
+++ bin/make-all.sh
@@ -2,5 +2,6 @@
 ./make.sh ../dist/curl/curl.js ../src/curl.js
 
 # make other versions of curl
-./make.sh ../dist/curl-with-js-and-domReady/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/plugin/js.js
-./make.sh ../dist/curl-for-dojo1.6/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/dojo16Compat.js
+./make.sh ../dist/curl-with-js-and-domReady/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/plugin/js.js ../src/curl/plugin/domReady.js
+./make.sh ../dist/curl-for-dojo1.6/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/dojo16Compat.js ../src/curl/plugin/domReady.js
+./make.sh ../dist/curl-kitchen-sink/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/dojo16Compat.js ../src/curl/plugin/js.js ../src/curl/plugin/text.js ../src/curl/plugin/i18n.js ../src/curl/plugin/domReady.js
