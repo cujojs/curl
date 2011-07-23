2c6bdd10896c2b353d30c839ccebb1b63835930a
unscriptable <john@e-numera.com>
2011-07-23 08:57:33 -0400

    much more robust async! plugin

--- src/curl/plugin/async.js
+++ src/curl/plugin/async.js
@@ -3,7 +3,31 @@
 
 	async plugin takes another module as it's resource and defers callback
 	until that module is complete.  the module must return a promise-like
-	object (i.e. has a then method)
+	object (i.e. has a then method). usage:
+
+	// module that depends upon a deferred (async) resource:
+	define(['async!deferredResource'], function (deferredResource) {
+		// use deferredResource
+	});
+	
+	// deferredResource:
+	define(function () {
+		var resolved, queue, undef;
+		queue = [];
+		function fetchResource () {
+			// go get the resource and call loaded when done
+		}
+		function loaded (resource) {
+			var callback;
+			resolved = resource;
+			while ((callback = queue.pop()) callback(resolved);
+		}
+		return {
+			then: function (callback, errback) {
+				if (resolved != undef) callback(resolved); else queue.push(callback);
+			}
+		};
+	});
 
 */
 define(function () {
@@ -11,19 +35,49 @@ define(function () {
 	return {
 
 		'load': function (resourceId, require, callback, config) {
-			require(resourceId, function (module) {
-				if (typeof module.then == 'function') {
-					module.then(function (resolved) {
-						callback(arguments.length > 0 ? resolved : module);
-					});
+
+			function resolved (resource) {
+				// return the resource to the callback
+				if (typeof callback.resolve == 'function') {
+					// promise-like callback
+					callback.resolve(resource);
 				}
 				else {
-					// TODO: should we throw here instead?
-					callback(module);
+					// just a function
+					callback(resource);
+				}
+			}
+
+			function rejected (error) {
+				// report that an error happened
+				if (typeof callback.reject == 'function') {
+					// promise-like callback
+					callback.reject(error);
+				}
+				// no way to report errors if the callback is not a promise
+			}
+
+			// go get the module in the standard way
+			require([resourceId], function (module) {
+
+				if (typeof module.then == 'function') {
+					// promise-like module
+					module.then(
+						function (resource) {
+							if (arguments.length == 0) resource = module;
+							resolved(resource);
+						},
+						rejected
+					);
+				}
+				else {
+					// just a callback
+					resolved(module);
 				}
 			});
 		},
 
+		// for cram's analyze phase
 		'analyze': function (resourceId, api, addDep) {
 			addDep(resourceId);
 		}
