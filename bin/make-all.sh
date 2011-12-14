185eee654b3d449302ed5c970100ea65c3d61279
unscriptable <john@e-numera.com>
2011-12-14 14:02:11 -0500

    removed jquery shim since it's not needed for jquery 1.7.1

--- bin/make-all.sh
+++ bin/make-all.sh
@@ -6,4 +6,4 @@
 ./make.sh ../dist/curl-with-js-and-domReady/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/plugin/js.js ../src/curl/plugin/domReady.js
 ./make.sh ../dist/curl-for-dojo1.6/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/dojo16Compat.js ../src/curl/plugin/domReady.js
 ./make.sh ../dist/curl-kitchen-sink/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/dojo16Compat.js ../src/curl/plugin/js.js ../src/curl/plugin/text.js ../src/curl/plugin/async.js ../src/curl/plugin/css.js ../src/curl/plugin/domReady.js
-./make.sh ../dist/curl-for-jQuery1.7/curl.js ../src/curl.js ../src/curl/jQuery17Compat.js  ../src/curl/plugin/js.js ../src/curl/plugin/link.js
+./make.sh ../dist/curl-for-jQuery1.7/curl.js ../src/curl.js ../src/curl/plugin/js.js ../src/curl/plugin/link.js
