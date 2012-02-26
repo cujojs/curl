9c1aef3ffabb9b74eb97e27914f51f5db6666ee2
John Hann <john@e-numera.com>
2012-02-25 22:47:59 -0500

    fix test to reflect proper exports functionality

--- test/support/commonjs/this.js
+++ test/support/commonjs/this.js
@@ -1,6 +1,6 @@
 define(function (require, exports, module) {
 "use strict";
-	var messages = require('./folder/messages'),
+	var messages = require('./folder/non-cjs'),
 		mod = require('./module');
 	this.testMessage = messages.itWorks;
 	this._module = module;
