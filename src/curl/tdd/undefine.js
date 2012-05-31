13d689f57feba3a7e1f8a830a924435841b9affc
unscriptable <john@unscriptable.com>
2012-05-30 22:05:31 -0400

    attempt to remove script element (may not be working if browser is automatically making script.src absolute)

--- src/curl/tdd/undefine.js
+++ src/curl/tdd/undefine.js
@@ -8,8 +8,12 @@
  *
  */
 
-define(['curl/_privileged'], function (priv) {
-	var cache = priv['cache'];
+define(['curl/_privileged', 'require'], function (priv, require) {
+	var cache, cleanupScript;
+
+	cache = priv['cache'];
+
+	cleanupScript = typeof document != 'undefined' ? removeScript : noop;
 
 	/**
 	 * Removes a module from curl.js's cache so that it can
@@ -18,12 +22,34 @@ define(['curl/_privileged'], function (priv) {
 	 * @param moduleId {String|Array} the id of a module (or modules)
 	 */
 	return function undefine (moduleId) {
-		var ids, id;
+		var ids, id, url;
 
 		ids = [].concat(moduleId);
 		while ((id = ids.pop())) {
+			if (cache[id] instanceof priv.Promise) {
+				url = require.toUrl(cache[id].ctxId || id);
+			}
+			else {
+				url = require.toUrl(id);
+			}
 			delete cache[id];
+			cleanupScript(url);
 		}
 	};
+
+	function removeScript (url) {
+		var rx, scripts, i, script;
+		rx = new RegExp(url + '($|\\.)', 'i');
+		scripts = document.getElementsByTagName('script');
+		i = 0;
+		while ((script = scripts[i++])) {
+			if (rx.test(script.src)) {
+				script.parentNode.removeChild(script);
+				scripts = []; // all done!
+			}
+		}
+	}
+
+	function noop () {}
 
 });
