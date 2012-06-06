2920eda65c7903636c1db85b434b00b271f64c06
unscriptable <john@unscriptable.com>
2012-06-05 22:01:36 -0400

    added ability to specify optimization level

--- bin/make.sh
+++ bin/make.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+# get optional optimization instruction
+opt=$1
+if [ "${opt:0:2}" = "--" ]; then
+	opt=${opt:2}
+	shift
+else
+	opt=ADVANCED_OPTIMIZATIONS
+fi
+
 # grab the output file
 out=$1
 tmpfile=$(mktemp -t cram.XXXXXX)
@@ -7,12 +17,20 @@ tmpfile=$(mktemp -t cram.XXXXXX)
 shift
 
 echo "making $out"
+echo "optimization level is $opt"
 
 # concatenate all of the files to a temp file
 cat $@ | sed -e "s:\/\*==::g" -e "s:==\*\/::g" > "$tmpfile"
 
-# compile them to the output file
-./compile.sh "$tmpfile" > "$out"
+if [ "$opt" = "NONE" ]; then
+	# cat files to the output file
+	cat "$tmpfile" > "$out"
+else
+	# compile files to the output file
+	./compile.sh "$tmpfile" "$opt" > "$out"
+fi
 
 # remove the temporary concatenated file
 rm "$tmpfile"
+
+echo "created $out"
