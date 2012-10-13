e6c6dd770c3f0c2dca72a72acd01ef3ca7c2bbbf
unscriptable <john@unscriptable.com>
2012-10-12 21:51:36 -0400

    restore suffixes/options in normalize()

--- src/curl/plugin/js.js
+++ src/curl/plugin/js.js
@@ -121,7 +121,7 @@ define(/*=='curl/plugin/js',==*/ ['curl/_privileged'], function (priv) {
 
 		'normalize': function (id, toAbsId, config) {
 			var end = id.indexOf('!');
-			return toAbsId(end > 0 ? id.substr(0, end) : id);
+			return toAbsId(end > 0 ? id.substr(0, end) : id) + id.substr(end);
 		},
 
 		'load': function (name, require, callback, config) {
