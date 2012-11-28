a11b2ab918e44496a99120c1cab3d8d8febc2e43
unscriptable <john@unscriptable.com>
2012-11-28 18:21:06 -0500

    extract fetchText from text! plugin into a reusable module

--- src/curl/plugin/text.js
+++ src/curl/plugin/text.js
@@ -8,55 +8,11 @@
  */
 
 /**
- * TODO: load xdomain text, too
+ * TODO: load xdomain text, too, somehow
  *
  */
 
-define(/*=='curl/plugin/text',==*/ function () {
-
-	var progIds = ['Msxml2.XMLHTTP', 'Microsoft.XMLHTTP', 'Msxml2.XMLHTTP.4.0'];
-
-	function xhr () {
-		if (typeof XMLHttpRequest !== "undefined") {
-			// rewrite the getXhr method to always return the native implementation
-			xhr = function () { return new XMLHttpRequest(); };
-		}
-		else {
-			// keep trying progIds until we find the correct one, then rewrite the getXhr method
-			// to always return that one.
-			var noXhr = xhr = function () {
-					throw new Error("getXhr(): XMLHttpRequest not available");
-				};
-			while (progIds.length > 0 && xhr === noXhr) (function (id) {
-				try {
-					new ActiveXObject(id);
-					xhr = function () { return new ActiveXObject(id); };
-				}
-				catch (ex) {}
-			}(progIds.shift()));
-		}
-		return xhr();
-	}
-
-	function fetchText (url, callback, errback) {
-		var x = xhr();
-		x.open('GET', url, true);
-		x.onreadystatechange = function (e) {
-			if (x.readyState === 4) {
-				if (x.status < 400) {
-					callback(x.responseText);
-				}
-				else {
-					errback(new Error('fetchText() failed. status: ' + x.statusText));
-				}
-			}
-		};
-		x.send(null);
-	}
-
-	function error (ex) {
-		throw ex;
-	}
+define(/*=='curl/plugin/text',==*/ ['./fetchText'], function (fetchText) {
 
 	return {
 
@@ -75,5 +31,9 @@ define(/*=='curl/plugin/text',==*/ function () {
 		'pluginBuilder': './builder/text'
 
 	};
+
+	function error (ex) {
+		throw ex;
+	}
 
 });
