b0c069211f544303c0f8aa4ab98b2a0542a374e7
unscriptable <john@unscriptable.com>
2013-01-18 00:00:56 -0500

    add cram CommonJS Modules/1.1 plugin

--- src/curl/loader/cjsm11.js
+++ src/curl/loader/cjsm11.js
@@ -82,38 +82,40 @@ define(/*=='curl/loader/cjsm11',==*/ function () {
 		head.insertBefore(el, insertBeforeEl);
 	}
 
-	return {
-		'load': function (resourceId, require, callback, config) {
-			// TODO: extract xhr from text! plugin and use that instead (after we upgrade to cram.js)
-			require(['text!' + resourceId + '.js', 'curl/_privileged'], function (source, priv) {
-				var moduleMap;
+	wrapSource['load'] = function (resourceId, require, callback, config) {
+		// TODO: extract xhr from text! plugin and use that instead (after we upgrade to cram.js)
+		require(['text!' + resourceId + '.js', 'curl/_privileged'], function (source, priv) {
+			var moduleMap;
 
-				// find (and replace?) dependencies
-				moduleMap = priv['core'].extractCjsDeps(source);
-				//source = parseDepModuleIds(source, moduleMap, config.replaceRequires);
+			// find (and replace?) dependencies
+			moduleMap = priv['core'].extractCjsDeps(source);
+			//source = parseDepModuleIds(source, moduleMap, config.replaceRequires);
 
-				// get deps
-				require(moduleMap, function () {
+			// get deps
+			require(moduleMap, function () {
 
-					// wrap source in a define
-					source = wrapSource(source, resourceId, config['injectSourceUrl'] !== false && require.toUrl(resourceId));
+				// wrap source in a define
+				source = wrapSource(source, resourceId, config['injectSourceUrl'] !== false && require.toUrl(resourceId));
 
-					if (config['injectScript']) {
-						injectScript(source);
-					}
-					else {
-						//eval(source);
-						globalEval(source);
-					}
+				if (config['injectScript']) {
+					injectScript(source);
+				}
+				else {
+					//eval(source);
+					globalEval(source);
+				}
 
-					// call callback now that the module is defined
-					callback(require(resourceId));
+				// call callback now that the module is defined
+				callback(require(resourceId));
 
-				}, callback['error'] || function (ex) { throw ex; });
+			}, callback['error'] || function (ex) { throw ex; });
 
-			});
-		}
+		});
 	};
+
+	wrapSource['cramPlugin'] = '../cram/i18n';
+
+	return wrapSource;
 
 });
 
