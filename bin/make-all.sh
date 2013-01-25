f527f4b1aa761e359d6ac88a6f3b84b3c190665f
unscriptable <john@unscriptable.com>
2013-01-25 09:08:16 -0500

    add debug make option

--- bin/make-all.sh
+++ bin/make-all.sh
@@ -3,6 +3,9 @@
 # make simple, compiled curl.js
 ./make.sh ../dist/curl/curl.js ../src/curl.js
 
+# make curl-debug.js
+./make.sh --NONE ../dist/debug/curl.js ../src/curl.js ../src/curl/debug.js
+
 # make other versions of curl
 ./make.sh ../dist/curl-with-js-and-domReady/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/plugin/js.js ../src/curl/plugin/domReady.js
 ./make.sh ../dist/curl-for-dojo1.6/curl.js ../src/curl.js ../src/curl/domReady.js ../src/curl/shim/dojo16.js ../src/curl/plugin/domReady.js
