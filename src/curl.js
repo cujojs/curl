1e1115b653a16486034a69116957654f9559783d
unscriptable <john@unscriptable.com>
2013-02-03 11:17:00 -0500

    implement auto-define for non-module js files with .js extension

--- src/curl.js
+++ src/curl.js
@@ -933,13 +933,18 @@
 			core.loadScript(def,
 
 				function () {
-					var args = argsNet;
-					argsNet = undef; // reset it before we get deps
+					var args;
 
+					args = argsNet;
+					argsNet = undef; // reset it before we get deps
+					
 					// if our resource was not explicitly defined with an id (anonymous)
 					// Note: if it did have an id, it will be resolved in the define()
 					if (def.useNet !== false) {
 
+						// if the resource is a plain js file, auto-define it here
+						if (/\.js$/.test(def.id)) define(def.id, undef);
+						
 						// if !args, nothing was added to the argsNet
 						if (!args || args.ex) {
 							def.reject(new Error(((args && args.ex) || 'define() missing or duplicated: ' + def.url)));
